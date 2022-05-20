a701795de6e56d51f11d72095e6293a3f0a0a00f
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-05-20 17:57:33 +0200

    progress on score system

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
+    println!(
+        "You start with 15 points. Try to guess the correct number before running out of points."
+    );
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let default_score = 15;
+    let mut score = &default_score;
 
     loop {
         println!("Please input your guess.");
@@ -29,9 +34,12 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! You kept {} points");
                 break;
             }
         }
+        println!("You lost a point.");
+        score = score - 1;
+        println!("You now have {} points left.", score);
     }
 }
